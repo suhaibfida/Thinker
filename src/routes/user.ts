3f8b083cf8beb31471535564adb9aaa163705f37
suhaibfida <suhaibfida33@outlook.com>
2025-08-07 16:06:30 +0530

    signin endpoint

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,6 +1,10 @@
 import express from "express";
 import { Router } from "express";
 import { UserModel } from "../db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+const jSecret = process.env.jSecret!;
 const userRouter = Router();
 userRouter.use(express.json());
 userRouter.post("/signup", async (req, res) => {
@@ -14,7 +18,29 @@ userRouter.post("/signup", async (req, res) => {
     meassage: "User Signed Up",
   });
 });
-userRouter.post("/signin", (req, res) => {});
+userRouter.post("/signin", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const existingUser = await UserModel.findOne({
+    username,
+    password,
+  });
+  if (existingUser) {
+    const token = jwt.sign(
+      {
+        id: existingUser._id,
+      },
+      jSecret
+    );
+    res.json({
+      token,
+    });
+  } else {
+    res.json({
+      message: "not found",
+    });
+  }
+});
 userRouter.post("/content", (req, res) => {});
 userRouter.get("/showcontent", (req, res) => {});
 userRouter.delete("/deletecontent", (req, res) => {});
